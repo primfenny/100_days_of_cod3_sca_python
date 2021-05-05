fa95477a7d867e26fc2da559ee24e3e6e75fe064
primfenny <ifennaokoye.n@gmail.com>
2021-05-05 07:36:55 -0700

    day 1 project

---
+++ day_1_of 100days_code.py
@@ -0,0 +1,8 @@
+print("welcome to the band name generator.")
+city = input("which city did you grow up in?\n")
+#city is a variable to hold the input value.
+print (city)
+pet = input("what is the name of your pet?\n")
+#combine the name of the city and the pet to for a band name using concatination
+print("your band  could be "+ city + ""+pet)
+
